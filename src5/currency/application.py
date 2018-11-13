6810690e13d0aa86523a9103c4b677568a1dba0a
plowshare18 <onuj1234@gmail.com>
2018-11-13 16:25:37 +0900

    Added and finished reviewing src5

---
+++ src5/currency/application.py
@@ -0,0 +1,30 @@
+import requests
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+
+    # Query for currency exchange rate
+    currency = request.form.get("currency")
+    res = requests.get("http://data.fixer.io/api/latest?access_key=35f5ad7056f50ad8f610a49200a67cf0&format=1",
+                       params={"symbols": currency})
+
+    # Make sure request succeeded
+    if res.status_code != 200:
+        return jsonify({"success": False})
+
+    # Make sure currency is in response
+    data = res.json()
+    if currency not in data["rates"]:
+        return jsonify({"success": False})
+
+    return jsonify({"success": True, "rate": data["rates"][currency]})
